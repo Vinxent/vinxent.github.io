4854e336fc5ed04b29e99dd68ec7b6984e687c42
scheng20 <chengsheena25@gmail.com>
2019-12-21 17:16:49 -0800

    added all pages AND image assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'welcome/home'
 
   get 'home', to: 'welcome#home'
+  get 'about-us', to: 'welcome#about-us'
+  get 'table-tennis', to: 'welcome#table-tennis'
+  get 'team', to: 'welcome#team'
+  get 'events', to: 'welcome#events'
+  get 'shop', to: 'welcome#shop'
+  get 'sponsors', to: 'welcome#sponsors'
+  get 'contact-us', to: 'welcome#contact-us'
+  get 'join-us', to: 'welcome#join-us'
 
-  root 'welcome#index'
+
+  root 'welcome#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
